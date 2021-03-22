1269d937f08efdfb540820609dd8f7e8adbe33e1
Netszky1 <chigotjulien@gmail.com>
2021-03-22 15:19:30 +0100

    Main menu

--- leveltwo/display.py
+++ leveltwo/display.py
@@ -1,5 +1,5 @@
 import pygame # Moteur 2D
-import pygame_menu
+import pygame_menu #Créer les Menus
 import os # Pour importer des fichiers
 import time # Pour le timer qui servira a vérifier les actions dans le jeu
 pygame.init()
@@ -11,21 +11,30 @@ HEIGHT = 1080 # Hauteur Ecran
 style = pygame.font.SysFont('Comic',50)
 
 running = True
-
-timer = pygame.time.Clock()
+# timer = pygame.time.Clock()
+def Play():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    menu = pygame_menu.Menu(600, 600, 'LevelTwo',theme=pygame_menu.themes.THEME_SOLARIZED) #Fenetre Menu
+    menu.add.button("Bienvenue" + " " + input.get_value(), Play) #Bouton Jouer
+    pygame.display.set_caption('LevelTwo') #Titre Fenetre
+    menu.mainloop(screen)
 
 #Affichage Ecran Menu Principal
 def displayMenu():
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    menu = pygame_menu.Menu(600, 400, 'Welcome',theme=pygame_menu.themes.THEME_SOLARIZED)
-    menu.add.text_input('Name :', default='Nouveau')
-    menu.add.button('Play')
-    menu.add.selector('Niveaux :', [('Niveau 1', 1), ('Niveau 2', 2), ('Niveau Aleatoire', 3)])
-    menu.add.button('Quit', pygame_menu.events.EXIT)
+    global input
+    screen = pygame.display.set_mode((WIDTH, HEIGHT)) #Creer la Fenetre selon paramètre
+    menu = pygame_menu.Menu(600, 600, 'LevelTwo',theme=pygame_menu.themes.THEME_SOLARIZED) #Fenetre Menu
+    input = menu.add.text_input('Name :', default='Nom') #Texte input Nom Joueur
+    menu.add.button('Jouer', Play) #Bouton Jouer
+    menu.add.button('Editer', Play) #Bouton Jouer
+    menu.add.button('Créer', Play) #Bouton Jouer
+    menu.add.selector('Niveaux :', [('Niveau 1', 1), ('Niveau 2', 2), ('Niveau Aleatoire', 3)]) #Selecteur Niveaux
+    menu.add.button('Quit', pygame_menu.events.EXIT) #Bouton Quitter
     pygame.display.set_caption('LevelTwo') #Titre Fenetre
-    
-    pygame.display.update() #Update screen
     menu.mainloop(screen)
+
+    
+    
     
             
 
