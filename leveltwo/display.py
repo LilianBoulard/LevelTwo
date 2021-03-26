cb07502e8a529517cba99106805f124185fbfe1b
Lilian <lilian@boulard.fr>
2021-03-26 12:47:40 +0100

    Refactor display

--- leveltwo/display.py
+++ leveltwo/display.py
@@ -1,49 +1,90 @@
-import pygame # Moteur 2D
-import pygame_menu #Créer les Menus
-import os # Pour importer des fichiers
-import time # Pour le timer qui servira a vérifier les actions dans le jeu
+import os
+import time
+
+import pygame
+import pygame_menu
+
+from typing import Tuple
+
+
 pygame.init()
 
-#Paramètre Ecran
-WIDTH = 1920 #Largeur Ecran
-HEIGHT = 1080 # Hauteur Ecran
-#Parametre Ecriture
-style = pygame.font.SysFont('Comic',50)
+# Screen settings
+screen_width = 960
+screen_height = 540
+screen_size = (screen_width, screen_height)
+# Set font
+style = pygame.font.SysFont('calibri', 50)
 
-running = True
 # timer = pygame.time.Clock()
 
-#Ecran de Labyrinthe
-def play():
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    menu = pygame_menu.Menu(600, 600, 'LevelTwo',theme=pygame_menu.themes.THEME_SOLARIZED) #Fenetre Menu
-    menu.add.button("Bienvenue" + " " + input.get_value(), Play) #Bouton Jouer
-    menu.add.button('Retour', display_menu) #Bouton Jouer
-    pygame.display.set_caption('LevelTwo') #Titre Fenetre
-    menu.mainloop(screen)
 
-#Affichage Ecran Menu Principal
-def display_menu():
-    global input
-    screen = pygame.display.set_mode((WIDTH, HEIGHT)) #Creer la Fenetre selon paramètre
-    menu = pygame_menu.Menu(600, 600, 'LevelTwo',theme=pygame_menu.themes.THEME_SOLARIZED) #Fenetre Menu
-    input = menu.add.text_input('Name :', default='Nom') #Texte input Nom Joueur
-    menu.add.button('Jouer', Play) #Bouton Jouer
-    menu.add.button('Editer', Play) #Bouton Jouer
-    menu.add.button('Créer', Play) #Bouton Jouer
-    menu.add.selector('Niveaux :', [('Niveau 1', 1), ('Niveau 2', 2), ('Niveau Aleatoire', 3)]) #Selecteur Niveaux
-    menu.add.button('Quit', pygame_menu.events.EXIT) #Bouton Quitter
-    pygame.display.set_caption('LevelTwo') #Titre Fenetre
-    menu.mainloop(screen)
+class Display:
 
-    
-    
-    
-            
+    """
+    Parameters
+    ----------
+
+    screen_size: Tuple[int, int]
+        A 2-tuple containing (1) the screen width and (2) the screen height.
+
+    """
+
+    def __init__(self, screen_size: Tuple[int, int]):
+        self.screen_size = screen_size
+        self.theme: pygame_menu.themes.Theme = pygame_menu.themes.THEME_SOLARIZED
+        self.running = True
+
+    def set_theme(self, new_theme: pygame_menu.themes.Theme):
+        self.theme = new_theme
+
+    def get_screen(self):
+        return pygame.display.set_mode(self.screen_size)
+
+    def play(self, user_input):
+        screen = self.get_screen()
+        menu = pygame_menu.Menu('LevelTwo', *self.screen_size, theme=self.theme)
+        menu.add.button(f"Welcome {user_input.get_value()}", self.play)
+        menu.add.button('Back to main menu', self.display_menu)
+        pygame.display.set_caption('LevelTwo')
+        menu.mainloop(screen)
+
+    def edit(self):
+        pass
+
+    def create(self):
+        pass
+
+    def display_menu(self):
+        screen = self.get_screen()
+        menu = pygame_menu.Menu('LevelTwo', *self.screen_size, theme=self.theme)
+        user_input = menu.add.text_input('Name :', default='')  # Request player name
+
+        # Level selector
+        menu.add.selector('Level selected: ', [
+            ('First level', 1),
+            ('Second level', 2),
+            ('Random level', 3)
+        ])
+
+        play_args = [user_input]
+        menu.add.button('Play', self.play, *play_args)
+
+        edit_args = []
+        menu.add.button('Edit', self.edit, *edit_args)
+
+        edit_args = []
+        menu.add.button('Create', self.create, *edit_args)
+
+        menu.add.button('Quit', pygame_menu.events.EXIT)
+
+        pygame.display.set_caption('LevelTwo')
+        menu.mainloop(screen)
 
 
-while running:
-    display_menu()
+display = Display(screen_size)
+while display.running:
+    display.display_menu()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            display.running = False
