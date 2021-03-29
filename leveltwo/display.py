4a4d873039ab92383dce9d76c5530b66608072a1
Lilian <lilian@boulard.fr>
2021-03-29 08:56:28 +0200

    Split functions and implement selector functionality

--- leveltwo/display.py
+++ leveltwo/display.py
@@ -33,6 +33,7 @@ class Display:
     def __init__(self, screen_size: Tuple[int, int]):
         self.screen_size = screen_size
         self.theme: pygame_menu.themes.Theme = pygame_menu.themes.THEME_SOLARIZED
+        self.level_selected = 0
         self.running = True
 
     def set_theme(self, new_theme: pygame_menu.themes.Theme):
@@ -44,7 +45,7 @@ class Display:
     def play(self, user_input):
         screen = self.get_screen()
         menu = pygame_menu.Menu('LevelTwo', *self.screen_size, theme=self.theme)
-        menu.add.button(f"Welcome {user_input.get_value()}", self.play)
+        menu.add.button(f"Welcome {user_input.get_value()} to level {self.level_selected}", self.play)
         menu.add.button('Back to main menu', self.display_menu)
         pygame.display.set_caption('LevelTwo')
         menu.mainloop(screen)
@@ -56,25 +57,30 @@ class Display:
         pass
 
     def display_menu(self):
+
+        def on_selector_change(struct: tuple, selection: int):
+            print(struct, selection)
+            self.level_selected = selection
+
         screen = self.get_screen()
         menu = pygame_menu.Menu('LevelTwo', *self.screen_size, theme=self.theme)
-        user_input = menu.add.text_input('Name :', default='')  # Request player name
+        user_input = menu.add.text_input('Name :', default='')
 
         # Level selector
         menu.add.selector('Level selected: ', [
+            ('Random level', 0),
             ('First level', 1),
             ('Second level', 2),
-            ('Random level', 3)
-        ])
+        ], onchange=on_selector_change)
 
-        play_args = [user_input]
+        play_args = [user_input]  # Positional args to be passed to the callback below.
         menu.add.button('Play', self.play, *play_args)
 
         edit_args = []
         menu.add.button('Edit', self.edit, *edit_args)
 
-        edit_args = []
-        menu.add.button('Create', self.create, *edit_args)
+        create_args = []
+        menu.add.button('Create', self.create, *create_args)
 
         menu.add.button('Quit', pygame_menu.events.EXIT)
 
