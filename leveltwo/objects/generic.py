ee72aab3742853ab63e1f533ec6bbbe5bb292eb4
Lilian <lilian@boulard.fr>
2021-04-06 10:26:39 +0200

    Update structure

--- leveltwo/objects/generic.py
+++ leveltwo/objects/generic.py
@@ -5,10 +5,51 @@ Must be inherited by all other objects.
 
 import numpy as np
 
-from math import sqrt
+from math import sqrt, floor
+from typing import List, Tuple
 
 from leveltwo.enums.effects import LevelEffects, PlayerEffects
 from enum import Enum
+
+
+sprite_struct = List[Tuple[int, int, int, int]]
+sprite_sep = ', '  # Separator
+
+
+def read_sprite(sprite: str) -> sprite_struct:
+    """
+    Takes a sprite serialized as a string, and returns a list of tuples (pixels).
+    :param str sprite:
+    :return sprite_struct:
+    """
+    sprite_list = sprite.split(sprite_sep)
+    # We check if the remainder of the modulo by 4 is 0.
+    # 4 because we have 4 values per pixel: Red, Green, Blue, Alpha.
+    # See `docs/sprite_data_representation.md`.
+    if not len(sprite_list) % 4 == 0:
+        raise ValueError("Sprite's length should be divisible by 4.")
+
+    # Then, we group the values to get the list of pixels.
+    pixels: sprite_struct = []
+    offset = 0
+    for _ in range(len(sprite_list) // 4):
+        r, g, b, a = [int(i) for i in sprite_list[offset:offset + 4]]
+        pixels.append((r, g, b, a))
+        offset += 4
+
+    return pixels
+
+
+def serialize_sprite(sprite: sprite_struct) -> str:
+    flat_pixels: List[str] = []
+    for pixel in sprite:
+        flat_pixel = sprite_sep.join([str(v) for v in pixel])
+        flat_pixels.append(flat_pixel)
+
+    # Or as a comprehension:
+    # return sprite_sep.join([sprite_sep.join([str(v) for v in pixel]) for pixel in I])
+
+    return sprite_sep.join(flat_pixels)
 
 
 class GenericObject:
@@ -74,7 +115,7 @@ class GenericObject:
         self.sprite = self._compute_sprite()
 
     def _compute_sprite(self) -> np.array:
-        guessed_x = int(sqrt(len(self.appearance)))
+        guessed_x = floor(sqrt(len(self.appearance)))
         computed_size = guessed_x ** 2
         if computed_size != len(self.appearance):
             raise ValueError(f"Invalid size found: computed {computed_size}, expected {len(self.appearance)}")
