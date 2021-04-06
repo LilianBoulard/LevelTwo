03316e80689ab127527d48aad15e4d36240434ea
Lilian <lilian@boulard.fr>
2021-04-06 10:25:54 +0200

    Add utils

---
+++ leveltwo/utils.py
@@ -0,0 +1,14 @@
+def list_to_string(l: list) -> str:
+    """
+    :param list l: A list to serialize.
+    :return str: The same list, as a string.
+    """
+    return ', '.join(l)
+
+
+def string_to_list(s: str) -> list:
+    """
+    :param str s: A serialized list.
+    :return list: The actual list.
+    """
+    return s.split(', ')
