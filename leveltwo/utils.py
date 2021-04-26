37193502ef704a3994320b37d9238dfcd523c957
Lilian <lilian@boulard.fr>
2021-04-26 13:18:21 +0200

    Fix function

--- leveltwo/utils.py
+++ leveltwo/utils.py
@@ -12,7 +12,7 @@ def list_to_string(l: list) -> str:
     :param list l: A list to serialize.
     :return str: The same list, as a string.
     """
-    return ', '.join(l)
+    return ', '.join([str(v) for v in l])
 
 
 def string_to_list(s: str) -> list:
