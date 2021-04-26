f1e79ac16fb2727a77c5b62824e3c81813fdc6f0
Lilian <lilian@boulard.fr>
2021-04-26 18:20:02 +0200

    Reorder objects

--- leveltwo/database/init/default_objects.py
+++ leveltwo/database/init/default_objects.py
@@ -2,30 +2,17 @@ from ... import GenericObject
 from ...enums import Effects
 
 
-class Wall(GenericObject):
+class Empty(GenericObject):
     """
-    Wall : the character can't go past this obstacle.
-    """
-    def __init__(self, **kwargs):
-        name = "wall"
-        effect = Effects.PLAYER_STOP
-        traversable = False
-        min_instances = 4
-        max_instances = 10
-        super().__init__(3, name, effect, traversable, min_instances, max_instances, **kwargs)
-
-
-class Trap(GenericObject):
-    """
-    Trap : Kills the player when stepped on.
+    Empty: Default object, doesn't do anything special.
     """
     def __init__(self, **kwargs):
-        name = "trap"
-        effect = Effects.PLAYER_KILL
+        name = "empty"
+        effect = Effects.NONE
         traversable = True
-        min_instances = 4
-        max_instances = 10
-        super().__init__(4, name, effect, traversable, min_instances, max_instances, **kwargs)
+        min_instances = 10
+        max_instances = 1000
+        super().__init__(0, name, effect, traversable, min_instances, max_instances, **kwargs)
 
 
 class StartingPoint(GenericObject):
@@ -41,6 +28,29 @@ class StartingPoint(GenericObject):
         super().__init__(1, name, effect, traversable, min_instances, max_instances, **kwargs)
 
 
+class ArrivalPoint(GenericObject):
+    def __init__(self, **kwargs):
+        name = "arrival"
+        effect = Effects.LEVEL_FINISH
+        traversable = True
+        min_instances = 1
+        max_instances = 1
+        super().__init__(2, name, effect, traversable, min_instances, max_instances, **kwargs)
+
+
+class Wall(GenericObject):
+    """
+    Wall : the character can't go past this obstacle.
+    """
+    def __init__(self, **kwargs):
+        name = "wall"
+        effect = Effects.PLAYER_STOP
+        traversable = False
+        min_instances = 4
+        max_instances = 10
+        super().__init__(3, name, effect, traversable, min_instances, max_instances, **kwargs)
+
+
 class Mud(GenericObject):
     """
     Mud : slows the character when walked on.
@@ -51,27 +61,17 @@ class Mud(GenericObject):
         traversable = True
         min_instances = 4
         max_instances = 10
+        super().__init__(4, name, effect, traversable, min_instances, max_instances, **kwargs)
+
+
+class Trap(GenericObject):
+    """
+    Trap : Kills the player when stepped on.
+    """
+    def __init__(self, **kwargs):
+        name = "trap"
+        effect = Effects.PLAYER_KILL
+        traversable = True
+        min_instances = 4
+        max_instances = 10
         super().__init__(5, name, effect, traversable, min_instances, max_instances, **kwargs)
-
-
-class Empty(GenericObject):
-    """
-    Empty: Default object, doesn't do anything special.
-    """
-    def __init__(self, **kwargs):
-        name = "empty"
-        effect = Effects.NONE
-        traversable = True
-        min_instances = 10
-        max_instances = 1000
-        super().__init__(0, name, effect, traversable, min_instances, max_instances, **kwargs)
-
-
-class ArrivalPoint(GenericObject):
-    def __init__(self, **kwargs):
-        name = "arrival"
-        effect = Effects.LEVEL_FINISH
-        traversable = True
-        min_instances = 1
-        max_instances = 1
-        super().__init__(2, name, effect, traversable, min_instances, max_instances, **kwargs)
