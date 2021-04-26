18549318817d4a994543f023d6bf4e890dd4838a
Lilian <lilian@boulard.fr>
2021-04-26 16:39:23 +0200

    Add setup

---
+++ leveltwo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='leveltwo',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Our software engineering project, a simple maze game',
+    author='LilianBoulard',
+    license='MIT',
+)
