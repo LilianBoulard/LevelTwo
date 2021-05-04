ebf7f3333f3e851b3c606025109bf01c3b05f9a6
Lilian <lilian@boulard.fr>
2021-05-04 23:02:22 +0200

    Update names and add default color

--- leveltwo/sprites/object_to_color.py
+++ leveltwo/sprites/object_to_color.py
@@ -2,9 +2,10 @@ from enum import Enum
 
 
 class ObjectToColor(Enum):
+    default = (0, 0, 0)
     empty = (255, 255, 255)
-    start = (0, 127, 0)
-    arrival = (0, 0, 127)
+    startingpoint = (0, 127, 0)
+    arrivalpoint = (0, 0, 127)
     wall = (127, 127, 127)
     mud = (222, 184, 135)
     trap = (127, 0, 0)
