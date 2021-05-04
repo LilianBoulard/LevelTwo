9b58ec34641fd58d2b5d5a0165e17e6d7d759e30
Lilian <lilian@boulard.fr>
2021-05-04 23:06:24 +0200

    Remove unused config param

--- leveltwo/config.py
+++ leveltwo/config.py
@@ -4,14 +4,7 @@ This files contains common variables used across the project.
 
 """
 
-from typing import List, Tuple
-
 
 class Config:
 
     project_name: str = "LevelTwo"
-
-    pathfinding_algorithms: List[Tuple[str, int]] = [
-        ('None', 0),
-        ('A*', 1),
-    ]
