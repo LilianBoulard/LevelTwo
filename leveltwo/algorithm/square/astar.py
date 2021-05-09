02fc3d4b96b585deef34ab2966933b988a8303c0
Netszky1 <chigotjulien@gmail.com>
2021-05-09 23:57:23 +0200

    Astar testing should be ok

--- leveltwo/algorithm/square/astar.py
+++ leveltwo/algorithm/square/astar.py
@@ -8,13 +8,16 @@ from ...object import GenericObject
 
 class Cell:
     def __init__(self, x, y, traversable):
+        self.traversable = traversable
         self.x = x
         self.y = y
-        self.traversable = traversable
         self.parent = None
         self.g = 0
         self.h = 0
         self.f = 0
+
+    def __lt__(self, other):
+        return self.f < other.f
 
 
 class AstarSquare(Astar):
@@ -37,7 +40,7 @@ class AstarSquare(Astar):
             self.cells.append(Cell(x, y, traversable=cell.traversable))
 
     def get_heuristic(self, cell):
-        return 10 * (abs(cell.x - self.end.x) + abs(cell.x - self.end.y))
+        return 10 * (abs(cell.x - self.end.x) + abs(cell.y - self.end.y))
 
     def get_cell(self, x, y):
         return self.cells[x * self.s_y + y]
@@ -54,25 +57,55 @@ class AstarSquare(Astar):
             cells.append(self.get_cell(cell.x, cell.y + 1))
         return cells
 
+    def display_path(self):
+        cell = self.end
+        path = [(cell.x, cell.y)]
+        while cell.parent is not self.start:
+            cell = cell.parent
+            path.append((cell.x, cell.y))
+
+        path.append((self.start.x, self.start.y))
+        path.reverse()
+        return path
+
     def update_cell(self, adj, cell):
+        """Update adjacent cell.
+                @param adj adjacent cell to current cell
+                @param cell current cell being processed
+                """
         adj.g = cell.g + 10
         adj.h = self.get_heuristic(adj)
         adj.parent = cell
         adj.f = adj.h + adj.g
 
     def run_one_step(self):
+
+        """Solve maze, find path to ending cell.
+                @returns path or None if not found.
+                """
+        # add starting cell to open heap queue
         heapq.heappush(self.opened, (self.start.f, self.start))
         while len(self.opened):
+            # pop cell from heap queue
             f, cell = heapq.heappop(self.opened)
+            # add cell to closed list so we don't process it twice
             self.closed.add(cell)
+            # if ending cell, return found path
             if cell is self.end:
-                logging.info("End")
+                return self.display_path()
+            # get adjacent cells for cell
             adj_cells = self.get_adjacent(cell)
             for adj_cell in adj_cells:
-                if adj_cell.traversable and adj_cell not in self.closed:
-                    if(adj_cell.f, adj_cell) in self.opened:
+
+                if adj_cell.traversable is True and adj_cell not in self.closed:
+                    if (adj_cell.f, adj_cell) in self.opened:
+                        # if adj cell in open list, check if current path is
+                        # better than the one previously found
+                        # for this adj cell.
                         if adj_cell.g > cell.g + 10:
                             self.update_cell(adj_cell, cell)
-                else:
-                    self.update_cell(adj_cell, cell)
-                    heapq.heappush(self.opened, (adj_cell.f, adj_cell))
+                    else:
+                        self.update_cell(adj_cell, cell)
+                        print(cell.x, cell.y)
+                        # add adj cell to open list
+                        heapq.heappush(self.opened, (adj_cell.f, adj_cell))
