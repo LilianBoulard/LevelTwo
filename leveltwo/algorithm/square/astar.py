f01d50e563624c470d719f008be5f2f8d1a5c9b2
Netszky1 <chigotjulien@gmail.com>
2021-05-09 22:20:43 +0200

    Astar test

--- leveltwo/algorithm/square/astar.py
+++ leveltwo/algorithm/square/astar.py
@@ -30,7 +30,7 @@ class Astar(MazeSolvingAlgorithm):
         return 10 * (abs(cell[0] - self.end[0]) + abs(cell[1] - self.end[1]))
 
     def get_cell(self, x, y):
-        return self.cells[x * self.s_x + y]
+        return self.cells[[0] * self.s_x + [1]]
 
     def get_adjacent(self, cell):
         cells = []
@@ -41,7 +41,7 @@ class Astar(MazeSolvingAlgorithm):
         if cell.x > 0:
             cells.append(self.get_cell(cell[0] - 1, cell[1]))
         if cell.y < self.s_x:
-            cells.append(self.get_cell(cell[0], cell[1]+ 1))
+            cells.append(self.get_cell(cell[0], cell[1] + 1))
         return cells
 
     def update_cell(self, adj, cell):
@@ -51,14 +51,16 @@ class Astar(MazeSolvingAlgorithm):
         adj.f = adj.h + adj.g
 
     def run_one_step(self):
-        heapq.heappush(self.opened, (self.start[0], self.start))
+        heapq.heappush(self.opened, (self.start.f, self.start))
         while len(self.opened):
             f, cell = heapq.heappop(self.opened)
             self.closed.add(cell)
+            if cell is self.end:
+                print("END")
             adj_cells = self.get_adjacent(cell)
             for adj_cell in adj_cells:
-                if adj_cell == 4 and adj_cell not in self.closed:
-                    if adj_cell > 10:
+                if (adj_cell != 4 or 6) and adj_cell not in self.closed:
+                    if adj_cell.f > cell.g + 10:
                         self.update_cell(adj_cell, cell)
                 else:
                     self.update_cell(adj_cell, cell)
